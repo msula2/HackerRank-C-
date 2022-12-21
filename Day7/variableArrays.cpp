1ac2674f7171be4c47c32acd83ad5993baaac7f8
hamdan <hamdan@7gfuse.com>
2022-12-21 17:08:28 +0500

    Scaning inputs, working

--- Day7/variableArrays.cpp
+++ Day7/variableArrays.cpp
@@ -11,16 +11,16 @@ int main() {
     // n is the number of variable sized arrays
     //q is the number of queries
     int n, q = 0;
-    scanf("%d %d", &n, &q);
+    scanf("%d %d\n", &n, &q);
     
     //Create array containing vectors
     vector<int> vecArr[n];
     //Read input into vectors because of varying length
     int element = 0;
+    char ch;
     for(int numArr = 0 ; numArr < n ; numArr++){
-        while(scanf("%d", &element)){  
+        while(scanf("%d", &element) && (ch = getchar()) != '\n'){  
             vecArr[numArr].push_back(element);
-            cout << vecArr[numArr].back() << " ";
         }
     }
     return 0;
